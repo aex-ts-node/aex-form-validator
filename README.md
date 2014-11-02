7111a39e6cd43ef05f5eb1bda7ef27330dc1bda8
renwenxin <renwenxin@e-carehome.com>
2014-11-02 13:26:25 +0800

    upated readme

--- README.md
+++ README.md
@@ -2,3 +2,35 @@ sails-params
 ============
 
 params extraction for sails
+
+
+
+
+
+```javascript
+
+//Inside sails controller
+module.exports = {
+  add: function (req, res) {
+
+    var filter = require("sails-params");
+
+    var params = [
+      {name: 'key', alias: 'alias'},
+      'key1'
+    ];
+    var data = {};
+
+    if (!filter.extract(req, data, params)) {
+      return false;
+    }
+    
+    //You will have
+    //data.alias
+    //data.key1
+    //assigned
+
+  }
+}
+
+```
--- test.js
+++ test.js
@@ -1,44 +1,42 @@
-
 var assert = require("assert")
-describe('sails-params', function(){
-  var filter =  require('./index');
+describe('sails-params', function () {
+  var filter = require('./index');
   var req = {
     data: {
       'a': 100,
       'b': 200,
       'c': 300
     },
-    param: function(k) {
+    param: function (k) {
       return req.data[k];
     }
   };
-
 
   var params = ['a', 'b', {name: 'c', alias: 'd'}];
 
   var data = {};
 
-  describe('#extract()', function(){
-    it('should return false when the req is not present', function(){
+  describe('#extract()', function () {
+    it('should return false when the req is not present', function () {
       assert.equal(false, filter.extract(null, data, params));
     });
 
-    it('should return false when the data is not present', function(){
+    it('should return false when the data is not present', function () {
       assert.equal(false, filter.extract(req, null, params));
     });
 
-    it('should return false when the params is not present', function(){
+    it('should return false when the params is not present', function () {
       assert.equal(false, filter.extract(req, data, null));
     });
 
-    it('should return true when all the params are present', function(){
+    it('should return true when all the params are present', function () {
       assert.equal(true, filter.extract(req, data, params));
     });
 
-    it('should be assigned after ', function(){
-      assert.equal(100,  data.a);
-      assert.equal(200,  data.b);
-      assert.equal(300,  data.d);
+    it('should be assigned after ', function () {
+      assert.equal(100, data.a);
+      assert.equal(200, data.b);
+      assert.equal(300, data.d);
     })
   })
 })
