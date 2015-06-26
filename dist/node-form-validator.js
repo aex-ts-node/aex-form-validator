e224c894211ab4334718750e84f5cba7ec514f5d
calidion <calidion@gmail.com>
2015-06-26 18:05:53 +0800

    updated to 0.1.1

--- dist/node-form-validator.js
+++ dist/node-form-validator.js
@@ -142,6 +142,63 @@ module.exports = {
    */
   count: function (o) {
     return Object.keys(o).length;
+  },
+  josn: {
+    /**
+     * Http request data extraction & validation
+     *
+     * @param json                - Json object
+     * @param confs                 - Validator confs
+     *                              //validator configuration
+     *                              {
+     *                                 type: ['string', 'email', 'phone', 'ip', 'url'],
+     *                                 maxLength: 10,
+     *                                 minLength: 10,
+     *                                 locale: 'zh-CN',
+     *                                 matches: 'paramName'
+     *                                 required: true/false,
+     *                                 options:
+     *                              }
+     * @returns {boolean}
+     */
+
+    extract: function (json, confs) {
+      if (!confs || !json) {
+        return false;
+      }
+      var data = {};
+      var params = [];
+      var ignores = [];
+      for (var key in confs) {
+        if (confs[key].ignore) {
+          ignores.push(key);
+        }
+        if (confs[key].alias) {
+          ignores.push(key);
+          params.push({name: key, alias: confs[key].alias});
+        }
+        params.push(key);
+      }
+      for (var i = 0; i < params.length; i++) {
+        var k = params[i];
+        var v = null;
+        var type = typeof k;
+        switch (type) {
+          case 'string':
+            v = json[k];
+            if (v) {
+              data[k] = v;
+            }
+            break;
+          case 'object':
+            v = json[k.name];
+            if (v) {
+              data[k.alias] = v;
+            }
+        }
+      }
+      return data;
+    },
   }
 };
 
