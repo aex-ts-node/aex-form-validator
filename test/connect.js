277de64c53da58afc5c263a344033cedb34a2fc6
calidion <calidion@gmail.com>
2016-06-30 16:46:17 +0800

    added connect

---
+++ test/connect.js
@@ -0,0 +1,17 @@
+'use strict';
+/* eslint space-before-function-paren: 0 */
+
+var assert = require('assert');
+var filter = require('../lib/index');
+
+describe('#asConnect()', function () {
+  it('should extract nothing', function (done) {
+    var req = {};
+    var res = {};
+    filter.asConnect(req, res, function name() {
+      assert(req.validate === filter.validate);
+      assert(req.extract === filter.extract);
+      done();
+    });
+  });
+});
